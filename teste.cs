0cf1e2319ba879b6e175cdf019a15675bb3a2ee9
Gian Condori Luna <francoguns16@gmail.com>
2022-09-09 20:03:15 -0300

    V1.0

---
+++ teste.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    //V1.0
+}
